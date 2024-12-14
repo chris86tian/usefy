1a7adfad99325b5c6a718bfcebe018c3c16cfeaf
Arslan Kamchybekov <arslankamcybekov7@gmail.com>
2024-12-14 13:06:11 -0600

    initial

---
+++ client/src/app/(nondashboard)/checkout/details/index.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import CoursePreview from "@/components/CoursePreview";
+import Loading from "@/components/Loading";
+import { useCurrentCourse } from "@/hooks/useCurrentCourse";
+import { useSearchParams } from "next/navigation";
+import React from "react";
+import SignUpComponent from "@/components/SignUp";
+import SignInComponent from "@/components/SignIn";
+
+const CheckoutDetailsPage = () => {
+  const { course: selectedCourse, isLoading, isError } = useCurrentCourse();
+  const searchParams = useSearchParams();
+  const showSignUp = searchParams.get("showSignUp") === "true";
+
+  if (isLoading) return <Loading />;
+  if (isError) return <div>Failed to fetch course data</div>;
+  if (!selectedCourse) return <div>Course not found</div>;
+
+  return (
+    <div className="checkout-details">
+      <div className="checkout-details__container">
+        <div className="checkout-details__preview">
+          <CoursePreview course={selectedCourse} />
+        </div>
+
+        {/* STRETCH FEATURE */}
+        <div className="checkout-details__options">
+          <div className="checkout-details__auth">
+            {showSignUp ? <SignUpComponent /> : <SignInComponent />}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CheckoutDetailsPage;
