934e1b36de59d2a00105bd13cc2a1a9521f1533c
Arslan Kamchybekov <arslankamcybekov7@gmail.com>
2025-02-06 09:15:07 -0600

    prod

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,7 +1,54 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.pexels.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+    domains: [
+      "expertize-bucket.s3.us-east-1.amazonaws.com",
+      "d2d2uxovkp6xho.cloudfront.net",
+    ],
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/courses/:path*",
+        destination:
+          "https://w10mtexzig.execute-api.us-east-1.amazonaws.com/dev/courses/:path*",
+      },
+      {
+        source: "/users/clerk/:path*",
+        destination:
+          "https://w10mtexzig.execute-api.us-east-1.amazonaws.com/dev/users/clerk/:path*",
+      },
+      {
+        source: "/transactions/:path*",
+        destination:
+          "https://w10mtexzig.execute-api.us-east-1.amazonaws.com/dev/transactions/:path*",
+      },
+      {
+        source: "/notifications/:path*",
+        destination:
+          "https://w10mtexzig.execute-api.us-east-1.amazonaws.com/dev/notifications/:path*",
+      },
+      {
+        source: "/commits/:path*",
+        destination:
+          "https://w10mtexzig.execute-api.us-east-1.amazonaws.com/dev/commits/:path*",
+      },
+      {
+        source: "/users/course-progress/:path*",
+        destination:
+          "https://w10mtexzig.execute-api.us-east-1.amazonaws.com/dev/users/course-progress/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
