3ac66a34d87fa78fd973096735453767404a6a87
Arslan Kamchybekov <arslankamcybekov7@gmail.com>
2025-02-06 09:06:07 -0600

    prod

--- client/next.config.js
+++ client/next.config.js
@@ -16,30 +16,71 @@ const nextConfig = {
   },
   async rewrites() {
     return [
+      // Catch requests to "/courses" and "/courses/:path*"
+      {
+        source: "/courses",
+        destination:
+          "https://w10mtexzig.execute-api.us-east-1.amazonaws.com/dev/courses",
+      },
       {
         source: "/courses/:path*",
         destination:
           "https://w10mtexzig.execute-api.us-east-1.amazonaws.com/dev/courses/:path*",
+      },
+
+      // Users Clerk API
+      {
+        source: "/users/clerk",
+        destination:
+          "https://w10mtexzig.execute-api.us-east-1.amazonaws.com/dev/users/clerk",
       },
       {
         source: "/users/clerk/:path*",
         destination:
           "https://w10mtexzig.execute-api.us-east-1.amazonaws.com/dev/users/clerk/:path*",
       },
+
+      // Transactions API
+      {
+        source: "/transactions",
+        destination:
+          "https://w10mtexzig.execute-api.us-east-1.amazonaws.com/dev/transactions",
+      },
       {
         source: "/transactions/:path*",
         destination:
           "https://w10mtexzig.execute-api.us-east-1.amazonaws.com/dev/transactions/:path*",
+      },
+
+      // Notifications API
+      {
+        source: "/notifications",
+        destination:
+          "https://w10mtexzig.execute-api.us-east-1.amazonaws.com/dev/notifications",
       },
       {
         source: "/notifications/:path*",
         destination:
           "https://w10mtexzig.execute-api.us-east-1.amazonaws.com/dev/notifications/:path*",
       },
+
+      // Commits API
+      {
+        source: "/commits",
+        destination:
+          "https://w10mtexzig.execute-api.us-east-1.amazonaws.com/dev/commits",
+      },
       {
         source: "/commits/:path*",
         destination:
           "https://w10mtexzig.execute-api.us-east-1.amazonaws.com/dev/commits/:path*",
+      },
+
+      // Users Course Progress API
+      {
+        source: "/users/course-progress",
+        destination:
+          "https://w10mtexzig.execute-api.us-east-1.amazonaws.com/dev/users/course-progress",
       },
       {
         source: "/users/course-progress/:path*",
