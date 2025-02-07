9387ca738bd5a392baca27e0953a486db3dfcb5b
Arslan Kamchybekov <arslankamcybekov7@gmail.com>
2025-02-06 19:43:16 -0600

    prod

--- client/next.config.ts
+++ client/next.config.ts
@@ -13,14 +13,14 @@ const nextConfig: NextConfig = {
    domains: [
      "expertize-bucket.s3.us-east-1.amazonaws.com",
      "d2d2uxovkp6xho.cloudfront.net",
-     "w10mtexzig.execute-api.us-east-1.amazonaws.com",
+     "https://oo9i2g3jc9.execute-api.us-east-1.amazonaws.com",
    ],
  },
  async rewrites() {
   return [
     {
       source: "/api/:path*",
-      destination: "https://w10mtexzig.execute-api.us-east-1.amazonaws.com/dev/:path*",
+      destination: "https://oo9i2g3jc9.execute-api.us-east-1.amazonaws.com/dev/:path*",
     },
   ];
 },
