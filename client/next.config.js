030d958b565c787597dc2a1617a908e14ef7e1e7
Arslan Kamchybekov <arslankamcybekov7@gmail.com>
2025-02-10 11:07:12 -0600

    fixed config issue

--- client/next.config.js
+++ client/next.config.js
@@ -1,29 +1,29 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      remotePatterns: [
-        {
-          protocol: "https",
-          hostname: "images.pexels.com",
-          port: "",
-          pathname: "/**",
-        },
-      ],
-      domains: [
-        "expertize-bucket.s3.us-east-1.amazonaws.com",
-        "d2d2uxovkp6xho.cloudfront.net",
-        "https://oo9i2g3jc9.execute-api.us-east-1.amazonaws.com"
-      ],
-    },
-    async rewrites() {
-      return [
-        {
-          source: "/api/:path*",
-          destination:
-            "https://oo9i2g3jc9.execute-api.us-east-1.amazonaws.com/dev/:path*",
-        },
-      ];
-    },
-  };
-  
-  export default nextConfig;
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.pexels.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+    domains: [
+      "expertize-bucket.s3.us-east-1.amazonaws.com",
+      "d2d2uxovkp6xho.cloudfront.net",
+      "oo9i2g3jc9.execute-api.us-east-1.amazonaws.com", // Removed "https://"
+    ],
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination:
+          "https://oo9i2g3jc9.execute-api.us-east-1.amazonaws.com/dev/:path*",
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig;
