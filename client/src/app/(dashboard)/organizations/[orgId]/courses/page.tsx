c684e6a4e8e8b2d3c9a6dfcc46291e463181ba4a
Arslan Kamchybekov <arslankamcybekov7@gmail.com>
2025-03-13 00:49:11 -0500

    fixed

--- client/src/app/(dashboard)/organizations/[orgId]/courses/page.tsx
+++ client/src/app/(dashboard)/organizations/[orgId]/courses/page.tsx
@@ -2,7 +2,6 @@
 
 import React from "react";
 import AdminCourses from "./_components/AdminCourses";
-import UserCourses from "./_components/UserCourses";
 import { useOrganization } from "@/context/OrganizationContext";
 import { useUser } from "@clerk/nextjs";
 
@@ -11,9 +10,10 @@ const Courses = () => {
   const { currentOrg } = useOrganization();
   const isAuthorized = currentOrg?.admins.some((admin) => admin.userId === user?.id) || currentOrg?.instructors.some((instructor) => instructor.userId === user?.id);
 
+  // TODO
   return (
     <>
-      {isAuthorized ? <AdminCourses /> : <UserCourses />}
+      {isAuthorized ? <AdminCourses /> : null }
     </>
   );
 };
