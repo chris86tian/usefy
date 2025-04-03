a69bdd2d0210d4ce92d5609dcdff7753adcd88b0
Amir Abaskanov <t@growthhungry.life>
2025-04-03 19:56:30 -0400

    Revert to Migration

--- client/next.config.js
+++ client/next.config.js
@@ -41,7 +41,7 @@ const nextConfig = {
         source: "/api/:path*",
         destination: process.env.NODE_ENV === "development" 
           ? "http://localhost:8001/:path*"
-          : "https://mhun775961.execute-api.us-east-1.amazonaws.com/prod/:path*",
+          : "https://mhun775961.execute-api.us-east-1.amazonaws.com/migration/:path*",
       },
     ];
   },
